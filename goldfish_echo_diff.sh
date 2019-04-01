2c3e45e877a8ef33deece2f229db225fe10762a4
Rick Venutolo <rick@venutolo.org>
2019-03-31 23:10:36 -0400

    Separate Groovy scripts and classes

--- goldfish_echo_diff.sh
+++ goldfish_echo_diff.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 readonly my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-groovy -cp "${my_dir}/src/" "${my_dir}/src/goldfish_echo_diff.groovy" "$@"
+groovy -cp "${my_dir}/src/" "${my_dir}/groovy_scripts/goldfish_echo_diff.groovy" "$@"
